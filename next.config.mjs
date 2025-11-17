9643d4c476c564b05c289afa05dc3827aea54d38
F3ZLoV <watmxb3794@naver.com>
2025-11-17 22:19:37 +0900

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/<repository-name>',
+  basePath: '/animated-portfolio',
   eslint: {
     ignoreDuringBuilds: true,
   },
